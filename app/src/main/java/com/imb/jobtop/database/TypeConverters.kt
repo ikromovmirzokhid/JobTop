12b7f4eb65d5859c693fad4feeb9cdd841b8f978
hiro <otashjumaev2000@gmail.com>
2020-12-05 20:48:22 +0500

    update

--- app/src/main/java/com/imb/jobtop/database/TypeConverters.kt
+++ app/src/main/java/com/imb/jobtop/database/TypeConverters.kt
@@ -5,15 +5,5 @@ import com.google.gson.Gson
 import java.util.*
 
 class TypeConverters {
-//
-//    @TypeConverter
-//    fun listToJson(value: ): String {
-//        return Gson().toJson(value)
-//    }
-//
-//    @TypeConverter
-//    fun jsonToList(value: String):  {
-//        val objects = Gson().fromJson(value, Array<SubTask>::class.java) as Array<SubTask>
-//        return objects.toList()
-//    }
+
 }
